f2cde93e07837d2d98c033a4b99b61a15d24c42c
tanabe-kazuma <k024g0040@g.neec.ac.jp>
2026-05-18 12:34:09 +0900

    Update level_editor.py
    サブメニューを追加

--- level_editor.py
+++ level_editor.py
@@ -14,11 +14,56 @@ bl_info = {
     "category":"Object"
 }
 
+# メニュー項目描画
+def draw_menu_manual(self,context):
+    #self : 呼び出し元のクラスインスタンス C++でいうthisポインタ
+
+    #トップバーのエディターメニューに項目追加
+    self.layout.operator("wm.url_open_preset",text="Manual", icon="HELP")
+
+#トップバーの拡張メニュー
+class TOPBAR_MT_my_menu(bpy.types.Menu):
+    #Blenderがクラスを識別する為の固有文字列
+    bl_idname = "TOPBAR_MT_my_menu"
+
+    #メニューのラベルとして表示される文字列
+    bl_label = "MYMenu"
+
+    #サブメニューの描画
+    def draw(self,context):
+
+        #トップバーのエディターメニューに項目を追加
+        self.layout.operetor("wm.url_open_preset", 
+            text="Manual", icon="HELP")
+        
+        #既存のメニューにサブメニューを追加
+        def submenu(self,context):
+
+            #ID指定でサブメニューを追加
+            self.layout.menu(TOPBAR_MT_my_menu)
+
+#Blenderに登録するクラスリスト
+classes =(
+    TOPBAR_MT_my_menu,
+)
+
 #アドオン有効時のコールバック
 def register():
+    #Blenderにクラスを登録
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
+    #メニューに項目追加
+    bpy.types.TOPBAR_MT_editor_menus.append(TOPBAR_MT_my_menu)
     print("レベルエディタが有効化されました")
 
 def unregister():
+    #メニューから項目削除
+    bpy.types.TOPBAR_MT_editor_menus.remove(TOPBAR_MT_my_menu)
+
+    #Blenderクラスから削除
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
     print("レベルエディタが無効化されました")
     
 if __name__ == "__main__":
