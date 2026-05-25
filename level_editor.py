e36fd7f399c97ea054d933c0f8acc19a7122f782
tanabe-kazuma <k024g0040@g.neec.ac.jp>
2026-05-25 12:19:38 +0900

    メニューを表示しマニュアルを見れる様にした

--- level_editor.py
+++ level_editor.py
@@ -27,25 +27,35 @@ class TOPBAR_MT_my_menu(bpy.types.Menu):
     bl_idname = "TOPBAR_MT_my_menu"
 
     #メニューのラベルとして表示される文字列
-    bl_label = "MYMenu"
+    bl_label = "MyMenu"
+
+    #著者表示用の文字列
+    bl_description= "拡張メニュー by" + bl_info["author"]
 
     #サブメニューの描画
     def draw(self,context):
 
-        #トップバーのエディターメニューに項目を追加
-        self.layout.operetor("wm.url_open_preset", 
-            text="Manual", icon="HELP")
+        #トップバーのエディターメニューに項目追加
+        self.layout.operator("wm.url_open_preset",text="Manual", icon="HELP")
         
-        #既存のメニューにサブメニューを追加
-        def submenu(self,context):
+    #既存のメニューにサブメニューを追加
+    def submenu(self,context):
 
-            #ID指定でサブメニューを追加
-            self.layout.menu(TOPBAR_MT_my_menu)
+        #ID指定でサブメニューを追加
+        self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
 
 #Blenderに登録するクラスリスト
 classes =(
     TOPBAR_MT_my_menu,
 )
+
+#オペレーター 頂点を伸ばす
+class MYADDON_OT_stretch_vertex(bpy.types.Operator):
+    bl_idname="myaddon.myaddon_ot_steretch_vertex"
+    bl_label="頂点を伸ばす"
+    bl_description="頂点座標を引っ張って伸ばします"
+    #リドゥ、アンドゥ可能オプション
+    bl_options={'REGISTER','UNDO'}
 
 #アドオン有効時のコールバック
 def register():
@@ -54,12 +64,12 @@ def register():
         bpy.utils.register_class(cls)
 
     #メニューに項目追加
-    bpy.types.TOPBAR_MT_editor_menus.append(TOPBAR_MT_my_menu)
+    bpy.types.TOPBAR_MT_editor_menus.append(TOPBAR_MT_my_menu.submenu)
     print("レベルエディタが有効化されました")
 
 def unregister():
     #メニューから項目削除
-    bpy.types.TOPBAR_MT_editor_menus.remove(TOPBAR_MT_my_menu)
+    bpy.types.TOPBAR_MT_editor_menus.remove(TOPBAR_MT_my_menu.submenu)
 
     #Blenderクラスから削除
     for cls in classes:
