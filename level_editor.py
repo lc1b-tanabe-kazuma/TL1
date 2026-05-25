27468b4396bbf9e9524e435091ece027e80056b2
tanabe-kazuma <k024g0040@g.neec.ac.jp>
2026-05-25 12:58:51 +0900

    メニューをから球体を作れる様にした

--- level_editor.py
+++ level_editor.py
@@ -14,11 +14,98 @@ bl_info = {
     "category":"Object"
 }
 
+# メニュー項目描画
+def draw_menu_manual(self,context):
+    #self : 呼び出し元のクラスインスタンス C++でいうthisポインタ
+
+    #トップバーのエディターメニューに項目追加
+    self.layout.operator("wm.url_open_preset",text="Manual", icon="HELP")
+
+#トップバーの拡張メニュー
+class TOPBAR_MT_my_menu(bpy.types.Menu):
+    #Blenderがクラスを識別する為の固有文字列
+    bl_idname = "TOPBAR_MT_my_menu"
+
+    #メニューのラベルとして表示される文字列
+    bl_label = "MyMenu"
+
+    #著者表示用の文字列
+    bl_description= "拡張メニュー by" + bl_info["author"]
+
+    #サブメニューの描画
+    def draw(self,context):
+
+         #トップバーのエディターメニューに項目を追加
+        self.layout.operator(MYADON_OT_create_sphere.bl_idname,
+            text=MYADON_OT_create_sphere.bl_label)
+
+        #トップバーのエディターメニューに項目を追加
+        self.layout.operator(MYADDON_OT_stretch_vertex.bl_idname,
+            text=MYADDON_OT_stretch_vertex.bl_label)
+
+        #トップバーのエディターメニューに項目追加
+        self.layout.operator("wm.url_open_preset",text="Manual", icon="HELP")
+        
+    #既存のメニューにサブメニューを追加
+    def submenu(self,context):
+
+        #ID指定でサブメニューを追加
+        self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
+
+#オペレーター 頂点を伸ばす
+class MYADDON_OT_stretch_vertex(bpy.types.Operator):
+    bl_idname="myaddon.myaddon_ot_steretch_vertex"
+    bl_label="頂点を伸ばす"
+    bl_description="頂点座標を引っ張って伸ばします"
+    #リドゥ、アンドゥ可能オプション
+    bl_options={'REGISTER','UNDO'}
+
+    #メニューを実行した時に呼ばれるコールバック関数
+    def execute(self,context):
+        bpy.data.objects["Cube"].data.vertices[0].co.x += 1.0
+        print("頂点を伸ばしました")
+
+        #オペレーターの命令終了
+        return {"FINISHED"}
+
+#オペレーター ICO球生成
+class MYADON_OT_create_sphere(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_create_object"
+    bl_label = "ICO球を生成します"
+    bl_options = {"REGISTER","UNDO"}
+
+    #メニューを実行した時に呼ばれる関数
+    def execute(self,context):
+        bpy.ops.mesh.primitive_ico_sphere_add()
+        print("ICO球を生成しました")
+
+        return{"FINISHED"}
+
+#C++でいうここからがメインループ 上がグローバル関数等
+#Blenderに登録するクラスリスト
+classes =(
+    MYADON_OT_create_sphere,
+    MYADDON_OT_stretch_vertex,
+    TOPBAR_MT_my_menu,
+)
+
 #アドオン有効時のコールバック
 def register():
+    #Blenderにクラスを登録
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
+    #メニューに項目追加
+    bpy.types.TOPBAR_MT_editor_menus.append(TOPBAR_MT_my_menu.submenu)
     print("レベルエディタが有効化されました")
 
 def unregister():
+    #メニューから項目削除
+    bpy.types.TOPBAR_MT_editor_menus.remove(TOPBAR_MT_my_menu.submenu)
+
+    #Blenderクラスから削除
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
     print("レベルエディタが無効化されました")
     
 if __name__ == "__main__":
